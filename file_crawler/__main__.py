0e18b9088f555f1ccad758c76df490a72cbdc351
Tharindu N <40334396+truethari@users.noreply.github.com>
2021-04-29 21:42:59 +0530

    user inputs

--- file_crawler/__main__.py
+++ file_crawler/__main__.py
@@ -1,8 +1,71 @@
+import os
 import sys
 import optparse
 
 from file_crawler.version           import __version__
 from file_crawler.application       import worker
+
+def _input_source_folder():
+    current_folder = os.getcwd()
+    while True:
+        source_folder = input("Source folder: ") or None
+        if source_folder is not None:
+            if not os.path.isdir(source_folder):
+                print("source folder: {} No such directory. Enter it again.".format(source_folder))
+                continue
+            else:
+                break
+        else:
+            current_as_source = input("Do you want to use {} \
+                                       as the source folder? [Y/n]\n".format(current_folder)) or 'n'
+            if current_folder.lower() == 'y':
+                source_folder = current_folder
+                break
+            else:
+                sys.exit()
+
+    return source_folder
+
+def _input_desination_folder(source_folder=None):
+    while True:
+        desination_folder = input("Desination folder: ") or None
+        if desination_folder is not None:
+            if not os.path.isdir(desination_folder):
+                print("desination folder: {} No such directory. Enter it again.".format(desination_folder))
+                continue
+            else:
+                break
+        else:
+            if current_folder != source_folder:
+                current_as_destination = input("Do you want to use {} \
+                                                as the desination folder? [Y/n]\n".format(current_folder)) or 'n'
+                if current_folder.lower() == 'y':
+                    desination_folder = current_folder
+                    break
+            else:
+                print("Enter a valid path to the destination folder.")
+
+def viewer():
+    while True:
+        try:
+            user_input = int(input("[1] Copy files from directory(tree) to directory\n"
+                                   "[2] Copy files from directory(not tree) to directory\n"
+                                   "[3] Copy files using a filelist\n"
+                                   "[4] Create filelist\n"
+                                   "[5] Exit"))
+            if user_input == 5:
+                sys.exit()
+            elif user_input > 5:
+                print("Invalid input!")
+                continue
+            else:
+                break
+
+        except ValueError:
+            print("Invalid input!")
+            continue
+
+    ###not completed###
 
 def main():
     usage = "usage: %prog [-s | --src] source_folder [-d | --dst] desination_folder \
